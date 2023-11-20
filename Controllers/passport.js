a6548f61ee3ba3141ad1e97563964e5e6eb28f14
abisek001 <abisek501@gmail.com>
2023-11-20 12:39:46 +0530

    env file updated

--- Controllers/passport.js
+++ Controllers/passport.js
@@ -1,13 +1,11 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const passport = require('passport');
-
-const GOOGLE_CLIENT_ID = "399063486393-0dll7nhrr75f7qrkoi19n8u7b2pl44jn.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
+require('dotenv').config();
  
 passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
